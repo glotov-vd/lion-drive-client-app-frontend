9c969f53cd867efda57387d0ccbbf17ed9e400c3
v.glotov <v.glotov@inforion.ru>
2024-12-09 15:24:35 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import logoGold from '/logo-gold.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={logoGold} className="logo" alt="Lion Drive" />
+        </a>
+      </div>
+      <h1>Lion Drive</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
